b1ae966aa91a46257d30589244386cde5e78b447
Jacob Williams <wjacobb@byui.edu>
2021-10-09 19:18:09 -0600

    Hello Caleb

--- src/holland/Main.java
+++ src/holland/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Caleb.");
+        System.out.println("Hello Caleb, my name is Jacob");
         //This comment is here to do the stretch goal
     }
 }
